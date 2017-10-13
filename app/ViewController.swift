2d261b5edfd3f3472caef20f416c88389a8dfdfe
Hamed.Gh <h.ghadirian@outlook.com>
2017-10-13 12:54:29 +0330

    test2

--- app/ViewController.swift
+++ app/ViewController.swift
@@ -15,6 +15,7 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view, typically from a nib.
     }
     //test
+    //test2
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
