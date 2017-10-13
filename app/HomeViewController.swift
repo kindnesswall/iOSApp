ac66f1252c6ef3e93ec892c1505bdba404e05cc6
Hamed.Gh <h.ghadirian@outlook.com>
2017-10-14 00:22:08 +0330

    Try to add request

--- app/HomeViewController.swift
+++ app/HomeViewController.swift
@@ -15,5 +15,58 @@ class HomeViewController: UIViewController {
 
         // Do any additional setup after loading the view.
     }
+    
+    func submitAction(sender: AnyObject) {
+        
+        //declare parameter as a dictionary which contains string as key and value combination.
+        let parameters = ["giftId": "hamed"] as [String: String]
+        
+        //create the url with NSURL
+        let url = NSURL(string: "http://match.ulc.ir/api/v1.0/")
+        
+        //create the session object
+        let session = URLSession.shared
+        
+        //now create the NSMutableRequest object using the url object
+        let request = NSMutableURLRequest(url: url! as URL)
+        request.httpMethod = "POST" //set http method as POST
+        
+        do {
+            request.httpBody = try JSONSerialization.data(withJSONObject: parameters, options: .prettyPrinted) // pass dictionary to nsdata object and set it as request body
+            
+        } catch let error {
+            print(error.localizedDescription)
+        }
+        
+        //HTTP Headers
+        request.addValue("application/json", forHTTPHeaderField: "Content-Type")
+        request.addValue("application/json", forHTTPHeaderField: "Accept")
+        
+        //create dataTask using the session object to send data to the server
+        let task = session.dataTask(with: request as URLRequest, completionHandler: { data, response, error in
+            
+            guard error == nil else {
+                return
+            }
+            
+            guard let data = data else {
+                return
+            }
+            
+            do {
+                //create json object from data
+                if let json = try JSONSerialization.jsonObject(with: data, options: .mutableContainers) as? [String: AnyObject] {
+                    print(json)
+                    // handle json...
+                }
+                
+            } catch let error {
+                print(error.localizedDescription)
+            }
+            
+        })
+        
+        task.resume()
+    }
 
 }
