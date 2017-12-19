b83fc8c7903eaa79558831d186bd54f9f15daaf3
Hamed.Gh <h.ghadirian@outlook.com>
2017-12-19 16:53:41 +0330

    Add icons

---
+++ app/views/RequestToAGiftTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  RequestToAGiftTableViewCell.swift
+//  app
+//
+//  Created by Hamed.Gh on 12/19/17.
+//  Copyright © 2017 Hamed.Gh. All rights reserved.
+//
+
+import UIKit
+
+class RequestToAGiftTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
