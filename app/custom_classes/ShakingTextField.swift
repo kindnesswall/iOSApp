ea14b2fd5107abb36a9c7d5cae8c7a696f65d7db
Hamed.Gh <h.ghadirian@outlook.com>
2018-12-03 20:34:12 +0330

    Shaking Textfield :)

---
+++ app/custom_classes/ShakingTextField.swift
@@ -0,0 +1,25 @@
+//
+//  ShakingTextField.swift
+//  app
+//
+//  Created by Hamed.Gh on 12/3/18.
+//  Copyright © 2018 Hamed.Gh. All rights reserved.
+//
+
+import UIKit
+
+class ShakingTextField: UITextField {
+
+    func shake() {
+        let animation = CABasicAnimation(keyPath: "position")
+        animation.duration = 0.05
+        animation.repeatCount = 5
+        animation.autoreverses = true
+        
+        animation.fromValue = NSValue(cgPoint: CGPoint(x: self.center.x - 4, y: self.center.y))
+        animation.toValue = NSValue(cgPoint: CGPoint(x: self.center.x + 4, y: self.center.y))
+        
+        self.layer.add(animation, forKey: "position")
+    }
+
+}
