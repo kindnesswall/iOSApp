988a62c3c3418c5edf4698b5f4ab019b02704519
Amir Hossein Zakizadeh <amirhossein.zakizadeh@gmail.com>
2019-06-14 13:37:18 +0430

    Chat Message Cell: Indicating if message is new (progress 1)

--- app/views/ChatViews/MessagesTableViewCell + Layout.swift
+++ app/views/ChatViews/MessagesTableViewCell + Layout.swift
@@ -6,18 +6,33 @@
 //  Copyright © 2019 Amir Hossein. All rights reserved.
 //
 
-import Foundation
+import UIKit
 
 extension MessagesTableViewCell {
+    
+    func layoutContainerStackView(){
+        let autoLayout = AutoLayoutHelper(view: self.containerStackView)
+        autoLayout.addConstraints(firstView: self.containerStackView, secondView: self.contentView, leading: 0, trailing: 0, top: 0, bottom: 0)
+    }
+    
+    func layoutIsNewMessageView(){
+        let autoLayout = AutoLayoutHelper(view: self.isNewMessageView)
+        autoLayout.addDimension(dimension: self.isNewMessageView.heightAnchor, equationType: .equal, constant: self.isNewMessageHeight)
+    }
+    
     func layoutMessageContentView(type: MessageUserType){
-        let autoLayout = AutoLayoutHelper(view: self.messageContentView)
+        
+        let subView = self.messageContentView
+        let superView = self.messageContainerView
+        
+        let autoLayout = AutoLayoutHelper(view: subView)
         switch type {
         case .others:
-            autoLayout.addConstraints(firstView: self.messageContentView, secondView: self.contentView, leading: margin, trailing: nil, top: margin, bottom: -margin)
-            autoLayout.addConstantConstraint(firstAnchor: self.messageContentView.trailingAnchor, secondAnchor: self.contentView.trailingAnchor, equationType: .lessThan, constant: -userMargin)
+            autoLayout.addConstraints(firstView: subView, secondView: superView, leading: margin, trailing: nil, top: margin, bottom: -margin)
+            autoLayout.addConstantConstraint(firstAnchor: subView.trailingAnchor, secondAnchor: superView.trailingAnchor, equationType: .lessThan, constant: -userMargin)
         case .user:
-            autoLayout.addConstraints(firstView: self.messageContentView, secondView: self.contentView, leading: nil, trailing: -margin, top: margin, bottom: -margin)
-            autoLayout.addConstantConstraint(firstAnchor: self.messageContentView.leadingAnchor, secondAnchor: self.contentView.leadingAnchor, equationType: .greaterThan, constant: userMargin)
+            autoLayout.addConstraints(firstView: subView, secondView: superView, leading: nil, trailing: -margin, top: margin, bottom: -margin)
+            autoLayout.addConstantConstraint(firstAnchor: subView.leadingAnchor, secondAnchor: superView.leadingAnchor, equationType: .greaterThan, constant: userMargin)
         }
     }
     
@@ -38,5 +53,27 @@ extension MessagesTableViewCell {
         autoLayout.addConstraints(firstView: self.stateLabel, secondView: self.timeLabel, leading: nil, trailing: nil, top: 0, bottom: 0)
         autoLayout.addConstraints(firstView: self.stateLabel, secondView: self.messageContentView, leading: nil, trailing: -textMargin/2, top: nil, bottom: nil)
         autoLayout.addDimension(dimension: self.stateLabel.widthAnchor, equationType: .lessThan, constant: stateLabelMaxWidth)
+    }
+    
+    func layoutIsNewMessageLabel(){
+        let autoLayout = AutoLayoutHelper(view: self.isNewMessageLabel)
+        autoLayout.addConstraints(firstView: self.isNewMessageLabel, secondView: self.isNewMessageView, leading: nil, trailing: nil, top: 0, bottom: 0)
+        autoLayout.addXAxisMultiplierConstraint(viewAnchor: self.isNewMessageLabel.centerXAnchor, superViewAnchor: self.isNewMessageView.centerXAnchor, equationType: .equal, multiplier: 0)
+        autoLayout.addDimension(dimension: self.isNewMessageLabel.widthAnchor, equationType: .equal, constant: self.isNewMessageLabelWidth)
+    }
+    
+    func layoutIsNewMessageLineLabel(line:UILabel,isLeft:Bool){
+        let autoLayout = AutoLayoutHelper(view: line)
+        autoLayout.addDimension(dimension: line.heightAnchor, equationType: .equal, constant: 1)
+        autoLayout.addYAxisMultiplierConstraint(viewAnchor: line.centerYAnchor, superViewAnchor: self.isNewMessageView.centerYAnchor, equationType: .equal, multiplier: 0)
+        
+        let superViewAnchor = isLeft ? self.isNewMessageView.leadingAnchor : self.isNewMessageView.trailingAnchor
+        autoLayout.addConstantConstraint(
+            firstAnchor: isLeft ? line.leadingAnchor : line.trailingAnchor, secondAnchor: superViewAnchor, equationType: .equal, constant: isLeft ? margin : -margin)
+
+        let centerLabelAnchor = isLeft ? self.isNewMessageLabel.leadingAnchor : self.isNewMessageLabel.trailingAnchor
+        autoLayout.addConstantConstraint(
+            firstAnchor: isLeft ? line.trailingAnchor : line.leadingAnchor, secondAnchor: centerLabelAnchor, equationType: .equal, constant: isLeft ? -margin : margin)
+        
     }
 }
--- app/views/ChatViews/MessagesTableViewCell.swift
+++ app/views/ChatViews/MessagesTableViewCell.swift
@@ -19,11 +19,21 @@ class MessagesTableViewCell: UITableViewCell {
     let userMargin:CGFloat = 50
     let textMargin:CGFloat = 10
     let stateLabelMaxWidth:CGFloat = 20
+    let isNewMessageHeight:CGFloat = 40
+    let isNewMessageLabelWidth:CGFloat = 100
+    
+    var containerStackView = UIStackView()
+    var isNewMessageView = UIView()
+    var messageContainerView = UIView()
     
     var messageContentView = UIView()
     var messageText = UILabel()
     var timeLabel = UILabel()
     var stateLabel = UILabel()
+    
+    var isNewMessageLabel = UILabel()
+    var isNewMessageLLineLabel = UILabel()
+    var isNewMessageRLineLabel = UILabel()
     
     override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
         super.init(style: style, reuseIdentifier: reuseIdentifier)
@@ -35,18 +45,40 @@ class MessagesTableViewCell: UITableViewCell {
     }
     
     func createElements(type: MessageUserType){
+        configContainerStackView(type: type)
+    }
+    
+    func configContainerStackView(type: MessageUserType){
+        self.containerStackView.axis = .vertical
+        self.contentView.addSubview(self.containerStackView)
+        self.layoutContainerStackView()
+        
+        configIsNewMessageView(type: type)
+        configMessageContainerView(type: type)
+    }
+    
+    func configIsNewMessageView(type: MessageUserType){
+        self.containerStackView.addArrangedSubview(self.isNewMessageView)
+        self.layoutIsNewMessageView()
+        self.configIsNewMessageLabels()
+    }
+    
+    func configMessageContainerView(type: MessageUserType){
+        self.containerStackView.addArrangedSubview(self.messageContainerView)
         configMessageContentView(type: type)
+    }
+    
+    func configMessageContentView(type: MessageUserType){
+        self.messageContentView.layer.cornerRadius = 10
+        self.messageContainerView.addSubview(self.messageContentView)
+        self.layoutMessageContentView(type: type)
+        
         configMessageTextLabel(type: type)
         configTimeLabel(type: type)
         configStateLabel(type: type)
     }
     
-    func configMessageContentView(type: MessageUserType){
-        self.messageContentView.layer.cornerRadius = 10
-        self.contentView.addSubview(self.messageContentView)
-        self.layoutMessageContentView(type: type)
-        
-    }
+    
     func configMessageTextLabel(type: MessageUserType){
         self.messageText.font = AppConst.Resource.Font.getRegularFont(size: 17)
         self.messageText.numberOfLines = 0
@@ -69,6 +101,24 @@ class MessagesTableViewCell: UITableViewCell {
         self.layoutStateLabel(type: type)
     }
     
+    func configIsNewMessageLabels(){
+        self.isNewMessageLabel.font = AppConst.Resource.Font.getRegularFont(size: 17)
+        self.isNewMessageLabel.textAlignment = .center
+        self.isNewMessageLabel.text = "پیغام جدید"
+        
+        let lineColor = UIColor.gray
+        self.isNewMessageLLineLabel.backgroundColor = lineColor
+        self.isNewMessageRLineLabel.backgroundColor = lineColor
+        
+        self.isNewMessageView.addSubview(self.isNewMessageLabel)
+        self.isNewMessageView.addSubview(self.isNewMessageLLineLabel)
+        self.isNewMessageView.addSubview(self.isNewMessageRLineLabel)
+        
+        self.layoutIsNewMessageLabel()
+        self.layoutIsNewMessageLineLabel(line: self.isNewMessageLLineLabel, isLeft: true)
+        self.layoutIsNewMessageLineLabel(line: self.isNewMessageRLineLabel, isLeft: false)
+    }
+    
     func updateUI(message:TextMessage?,messageType:MessageUserType){
         
         
@@ -77,6 +127,7 @@ class MessagesTableViewCell: UITableViewCell {
         }
         self.messageText.text = message.text
         self.timeLabel.text = AppLanguage.getNumberString(number: message.createdAt?.convertToDate()?.getClock() ?? "")
+        self.isNewMessageView.isHidden = true
         
         switch messageType {
         case .user:
