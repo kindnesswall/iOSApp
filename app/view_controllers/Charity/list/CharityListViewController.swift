cbfaa560532e74953bbe99d4b6342b70cf3adfcc
Hamed Ghadirian <h.ghadirian@outlook.com>
2019-07-10 00:47:42 +0200

    create charity model and 3 view controller for signup/edit, list and detail of charities

---
+++ app/view_controllers/Charity/list/CharityListViewController.swift
@@ -0,0 +1,19 @@
+//
+//  CharityListViewController.swift
+//  app
+//
+//  Created by Hamed Ghadirian on 10.07.19.
+//  Copyright © 2019 Hamed.Gh. All rights reserved.
+//
+
+import UIKit
+
+class CharityListViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+}
