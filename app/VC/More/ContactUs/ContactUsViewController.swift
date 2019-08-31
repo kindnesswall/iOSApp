4b035ab7156e70b04d4f44d4d830a0fa7829c857
Hamed Ghadirian <h.ghadirian@outlook.com>
2019-08-31 19:55:18 +0200

    Refactoring

---
+++ app/VC/More/ContactUs/ContactUsViewController.swift
@@ -0,0 +1,139 @@
+//
+//  ContactUsViewController.swift
+//  app
+//
+//  Created by Amir Hossein on 8/22/18.
+//  Copyright © 2018 Hamed.Gh. All rights reserved.
+//
+
+import UIKit
+
+class ContactUsViewController: UIViewController {
+    
+    
+    class ContactUsModel {
+        var image:UIImage?
+        var link:String?
+        
+        init(image:UIImage?,link:String?) {
+            self.image=image
+            self.link=link
+        }
+        
+        init(){
+            
+        }
+        
+    }
+    
+    var data = [ContactUsModel]()
+
+    @IBOutlet weak var collectionView: UICollectionView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        self.navigationItem.title = LocalizationSystem.getStr(forKey: LanguageKeys.contactUs)
+        
+        self.collectionView.register(cellType: ContactUsCollectionViewCell.self)
+
+        fillContactUsData()
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func criticismsAndSuggestionsBtnAction(_ sender: Any) {
+        let urlAddress = URIs.telegramLink
+        URLBrowser(urlAddress: urlAddress).openURL()
+    }
+    
+    func fillContactUsData(){
+        
+        let github = ContactUsModel(image: UIImage(named: "github") , link: URIs.githubLink)
+        
+        let gmail = ContactUsModel(image: UIImage(named: "gmail") , link: URIs.gmailLink)
+        
+        let instagram = ContactUsModel(image: UIImage(named: "instagram") , link: URIs.instagramLink)
+        
+        let telegram = ContactUsModel(image: UIImage(named: "telegram") , link: URIs.telegramLink)
+        
+        let facebook = ContactUsModel(image: UIImage(named: "facebook") , link: URIs.facebookLink)
+        
+        let website = ContactUsModel(image: UIImage(named: "website") , link: URIs.webSiteLink)
+        
+        data.append(github)
+        data.append(gmail)
+        data.append(instagram)
+        data.append(telegram)
+        data.append(facebook)
+        data.append(website)
+    }
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        self.navigationController?.navigationBar.setDefaultStyle()
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
+
+
+extension ContactUsViewController : UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return data.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: ContactUsCollectionViewCell.identifier, for: indexPath) as! ContactUsCollectionViewCell
+        
+        cell.setUI(data: data[indexPath.item])
+        
+        return cell
+    }
+    
+    
+}
+
+extension ContactUsViewController : UICollectionViewDelegate {
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        let item = self.data[indexPath.item]
+        guard let urlAddress = item.link else {
+            return
+        }
+        URLBrowser(urlAddress: urlAddress).openURL()
+    }
+}
+
+extension ContactUsViewController : UICollectionViewDelegateFlowLayout {
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
+        return 0
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        let collectionViewWidth = collectionView.frame.width
+        
+        let numberOfItemsInRow : CGFloat = 4
+        
+        let width = collectionViewWidth / numberOfItemsInRow
+        
+        let height = width
+        
+        return CGSize(width: width, height: height)
+        
+    }
+}
---
+++ app/VC/More/Statistic/StatisticViewController.swift
@@ -0,0 +1,120 @@
+//
+//  StatisticViewController.swift
+//  app
+//
+//  Created by Amir Hossein on 8/24/18.
+//  Copyright © 2018 Hamed.Gh. All rights reserved.
+//
+
+import UIKit
+
+class StatisticViewController: UIViewController , UITableViewDelegate,UITableViewDataSource {
+    
+    var statisticsKeys = [String]()
+    var statisticsValues = [Int]()
+
+    @IBOutlet weak var tableView: UITableView!
+    var initialLoadingIndicator:LoadingIndicator?
+    
+    lazy var apiRequest = ApiRequest(HTTPLayer())
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        self.navigationItem.title = LocalizationSystem.getStr(forKey: "StatisticViewController_title")
+        
+        self.tableView.register(type: StatisticTableViewCell.self)
+        self.initialLoadingIndicator=LoadingIndicator(view: self.view)
+        
+        fetchStatistics()
+        // Do any additional setup after loading the view.
+    }
+    
+    func startLoadingPage(){
+        self.initialLoadingIndicator?.startLoading()
+        self.tableView.hide()
+    }
+    func stopLoadingPage(){
+        self.initialLoadingIndicator?.stopLoading()
+        self.tableView.show()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        self.navigationController?.navigationBar.setDefaultStyle()
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return statisticsKeys.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: StatisticTableViewCell.identifier, for: indexPath) as! StatisticTableViewCell
+        cell.setUI(key: self.statisticsKeys[indexPath.item], value:AppLanguage.getNumberString(number: self.statisticsValues[indexPath.item].description))
+        return cell
+    }
+    
+    func fetchStatistics(){
+        
+        startLoadingPage()
+        
+        let input : APIEmptyInput? = nil
+        
+        apiRequest.getStatistics { [weak self](result) in
+            
+            self?.statisticsKeys = []
+            self?.statisticsValues = []
+            self?.stopLoadingPage()
+            
+            switch result{
+            case .failure(let appError):
+                print("Error")
+            case .success(let statisticsData):
+                print(statisticsData)
+                if let statistics = statisticsData.statistics {
+                    for (key , value) in statistics {
+                        self?.statisticsKeys.append(key)
+                        self?.statisticsValues.append(value)
+                    }
+                    self?.tableView.reloadData()
+                }
+            }
+        }
+//        APICall.request(url: APIURLs.getStatistics, httpMethod: .GET, input: input) { [weak self] (data, response, error)  in
+//            APICall.printData(data: data)
+            
+//            self?.statisticsKeys = []
+//            self?.statisticsValues = []
+//            self?.stopLoadingPage()
+//
+//            if let statisticsData = APICall.readJsonData(data: data, outputType: Statistics.self) {
+//                if let statistics = statisticsData.statistics {
+//                    for (key , value) in statistics {
+//                        self?.statisticsKeys.append(key)
+//                        self?.statisticsValues.append(value)
+//                    }
+//                    self?.tableView.reloadData()
+//                }
+//
+//            }
+//        }
+    
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
