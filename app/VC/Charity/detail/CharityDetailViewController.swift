d19c07f343e7e71c21cd29f66849439723b3a3a4
Hamed Ghadirian <h.ghadirian@outlook.com>
2019-08-31 20:50:33 +0200

    Add UI to show charity detail

--- app/VC/Charity/detail/CharityDetailViewController.swift
+++ app/VC/Charity/detail/CharityDetailViewController.swift
@@ -10,6 +10,30 @@ import UIKit
 
 class CharityDetailViewController: UIViewController {
 
+    @IBOutlet weak var charityImage: UIImageView!
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var addressLabel: UILabel!
+    @IBOutlet weak var editCharityInfoButton: UIButton!
+    
+    @IBAction func onTelephoneClicked(_ sender: Any) {
+    }
+    
+    @IBAction func onMobileClicked(_ sender: Any) {
+        
+    }
+    
+    @IBAction func onWebsiteClicked(_ sender: Any) {
+        
+    }
+    
+    @IBAction func onEmailClicked(_ sender: Any) {
+        
+    }
+    
+    @IBAction func onInstagramClicked(_ sender: Any) {
+        
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
